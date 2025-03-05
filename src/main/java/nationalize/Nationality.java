c85d6326c65ebc9fee7bc13f3e3719c4f9692bf7
Temujin Bayarsaikhan <temujin.bb9@gmail.com>
2025-03-05 10:53:49 +0100

    refactor: use Lombok in Nationality

--- src/main/java/nationalize/Nationality.java
+++ src/main/java/nationalize/Nationality.java
@@ -12,26 +12,7 @@ public class Nationality {
     private long count;
     private String name;
 
-    @SerializedName("country")
-    private List<Country> countries;
 
-    @lombok.Data
-    public static class Country {
-
-        private String countryId;
-        private float probability;
-
-        @Override
-        public String toString() {
-            return ReflectionToStringBuilder.toString(this, ToStringStyle.NO_CLASS_NAME_STYLE);
-        }
-
-    }
-
-    @Override
-    public String toString() {
-        return ReflectionToStringBuilder.toString(this, ToStringStyle.NO_CLASS_NAME_STYLE);
-    }
 
 
 
